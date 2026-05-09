47b302a80a947dd71182c45f4ce710cb8b06ef2b
Simon Gao <simongao398@gmail.com>
2026-05-09 00:47:07 -0400

    finalized quick searchfor creating list

--- arthive-frontend/src/lib/QuickSearch.tsx
+++ arthive-frontend/src/lib/QuickSearch.tsx
@@ -13,15 +13,19 @@ type QuickSearchProps = {
     setHasNextPage: (hasNextPage: boolean) => void
     loadMore: number
     setUser: (user: User) => void
+    setLoadMore: (loadMore: number) => void
 }
 // For small quick searches (e.g. when adding media to list or choosing which list to add to), no filters
-export default function QuickSearch({setSearchResults, searchType, limit, setHasNextPage, loadMore, setUser}: QuickSearchProps) {
+export default function QuickSearch({setSearchResults, searchType, limit, setHasNextPage, loadMore, setUser, setLoadMore}: QuickSearchProps) {
     if (searchType != "media" && searchType != "user" && searchType != "review") {
         throw new Error("Invalid search type")
     }
     const navigate = useNavigate()
     const [query, setQuery] = useState("")
+    const [currQuery, setCurrQuery] = useState("")
     const [cursor, setCursor] = useState<string | null>(null)
+
+    console.log(cursor)
 
     useEffect(() => {
         if (query.length > 0) {
@@ -30,18 +34,20 @@ export default function QuickSearch({setSearchResults, searchType, limit, setHas
             quickSearchQuery(query, searchType, cursor, limit, setUser, setHasNextPage, setCursor, setSearchResults, navigate, obtainQuickSearch)
             
         }
-    }, [query, loadMore])
+    }, [loadMore])
+
+    useEffect(() => {
+        setCursor(null)
+        setSearchResults([])
+        setLoadMore(loadMore + 1)
+    }, [query])
     const [obtainQuickSearch, {loading, error}] = useLazyQuery<SearchBarResponse, SearchBarInput>(SEARCH_BAR_QUERY)
     return (
         <div>
             {error && <div>{error.message}</div>}
             {loading && <div>Loading...</div>}
-            <input type="text" placeholder="Search" value={query} onChange={(e) => setQuery(e.target.value)} />
-            <button onClick={() => {
-                setCursor(null)
-                setSearchResults([])
-                quickSearchQuery(query, searchType, cursor, limit, setUser, setHasNextPage, setCursor, setSearchResults, navigate, obtainQuickSearch)
-            }}>Search</button>
+            <input type="text" placeholder="Search" value={currQuery} onChange={(e) => setCurrQuery(e.target.value)} />
+            <button disabled={query === currQuery} onClick={() => {setQuery(currQuery)}}>Search</button>
         </div>
     )
 }
--- arthive-frontend/src/pages/CreateListPage.tsx
+++ arthive-frontend/src/pages/CreateListPage.tsx
@@ -3,6 +3,7 @@ import type { User } from "../types/user_types"
 import type { CreateListInput, CreateListResponse } from "../types/mutations/create_edit_list_mutation"
 import { useMutation } from "@apollo/client/react"
 import { CREATE_LIST_MUTATION } from "../types/mutations/create_edit_list_mutation"
+import QuickSearch from "../lib/QuickSearch"
 export default function CreateListPage({setUser}: {setUser: (user: User) => void}) {
 
     const [addedMediaIds, setAddedMediaIds] = useState<string[]>([])
@@ -12,11 +13,18 @@ export default function CreateListPage({setUser}: {setUser: (user: User) => void
     const [ifPrivate, setIfPrivate] = useState<boolean>(false)
 
     const [createList, {loading, error}] = useMutation<CreateListResponse, CreateListInput>(CREATE_LIST_MUTATION)
+    
+    const [searchResults, setSearchResults] = useState<any[]>([])
+    const [hasNextPage, setHasNextPage] = useState<boolean>(false)
+    const [loadMore, setLoadMore] = useState<number>(10)
+
+    console.log(searchResults)
     return (
         <div>
             {error && <div>{error.message}</div>}
             {loading && <div>Loading...</div>}
             <h1>Create List</h1>
+            <QuickSearch setSearchResults={setSearchResults} searchType="media" limit={10} setHasNextPage={setHasNextPage} loadMore={loadMore} setUser={setUser} setLoadMore={setLoadMore} />
         </div>
     )
 }
