8526841b6dab854d862973e91bf177094bb39377
Simon Gao <simongao@Simons-MacBook-Air.local>
2026-05-29 23:07:03 -0400

    prod att v2

--- arthive-backend/config/initializers/sqs.rb
+++ arthive-backend/config/initializers/sqs.rb
@@ -1,7 +1,11 @@
-SQS_CLIENT = Aws::SQS::Client.new(
-    region: Rails.application.credentials.dig(:aws, :region),
-    access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-)
-
-SQS_NOTIFICATION_QUEUE_URL = Rails.application.credentials.dig(:aws, :sqs_url)
+if Rails.application.credentials.dig(:aws, :access_key_id).present?
+  SQS_CLIENT = Aws::SQS::Client.new(
+      region: Rails.application.credentials.dig(:aws, :region),
+      access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+  )
+  SQS_NOTIFICATION_QUEUE_URL = Rails.application.credentials.dig(:aws, :sqs_url)
+else
+  SQS_CLIENT = nil
+  SQS_NOTIFICATION_QUEUE_URL = nil
+end
